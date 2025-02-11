8fc274c29c17eee12b7830dd8a42c03c6f5e13a1
hugoalh <hugoalh@gmail.com>
2025-02-11 17:50:38 +0800

    Update resources

--- .dnt.ts
+++ .dnt.ts
@@ -9,6 +9,7 @@ await invokeDenoNodeJSTransformer({
 		"README.md"
 	],
 	entrypoints: configJSR.getExports(),
+	fixInjectedImports: true,
 	generateDeclarationMap: true,
 	mappings: {
 		"https://raw.githubusercontent.com/hugoalh/is-string-singleline-es/v1.0.4/mod.ts": {
